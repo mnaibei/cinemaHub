c76c29c00e924b3e1d2f4ae37abf17eabb2f8ae9
mnaibei <muchajulius@gmail.com>
2023-06-17 13:35:02 +0300

    Linters fix

--- src/modules/displayMovies.js
+++ src/modules/displayMovies.js
@@ -1,25 +1,24 @@
 const displayMovies = (movies) => {
   // getting container where data will be displayed
-  const resultsContainer = document.getElementById("results");
+  const resultsContainer = document.getElementById('results');
 
   // clearing previous loaded data
-  resultsContainer.innerHTML = "";
+  resultsContainer.innerHTML = '';
 
   // if no movies found from query show this message
   if (!movies) {
-    resultsContainer.innerHTML = "<p>No movies found.</p>";
+    resultsContainer.innerHTML = '<p>No movies found.</p>';
     return;
   }
 
   // loop through data found, create div and append to DOM
   movies.forEach((movie) => {
-    const movieCard = document.createElement("div");
-    movieCard.classList.add("movie-item");
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-item');
 
-    const posterUrl =
-      movie.Poster !== "N/A"
-        ? movie.Poster
-        : "https://images.unsplash.com/photo-1509281373149-e957c6296406?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1028&q=80";
+    const posterUrl = movie.Poster !== 'N/A'
+      ? movie.Poster
+      : 'https://images.unsplash.com/photo-1509281373149-e957c6296406?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1028&q=80';
 
     movieCard.innerHTML = `
       <img src=${posterUrl}>
@@ -32,11 +31,11 @@ const displayMovies = (movies) => {
             <div class="details-container" style="display: none;"></div>
       `;
 
-    const viewMoreBtn = movieCard.querySelector(".view-more-btn");
-    const detailsContainer = movieCard.querySelector(".details-container");
+    const viewMoreBtn = movieCard.querySelector('.view-more-btn');
+    const detailsContainer = movieCard.querySelector('.details-container');
 
     const fetchMovieDetails = async (imdbID) => {
-      const apiKey = "6d76a8b8";
+      const apiKey = '6d76a8b8';
       const endpoint = `https://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`;
 
       try {
@@ -50,11 +49,10 @@ const displayMovies = (movies) => {
 
     // Function to toggle visibility of details container
     function toggleDetailsVisibility(detailsContainer) {
-      detailsContainer.style.display =
-        detailsContainer.style.display === "none" ? "flex" : "none";
+      detailsContainer.style.display = detailsContainer.style.display === 'none' ? 'flex' : 'none';
     }
 
-    viewMoreBtn.addEventListener("click", () => {
+    viewMoreBtn.addEventListener('click', () => {
       if (detailsContainer.innerHTML) {
         // Details already fetched, toggle visibility
         toggleDetailsVisibility(detailsContainer);
@@ -71,8 +69,8 @@ const displayMovies = (movies) => {
           `;
           toggleDetailsVisibility(detailsContainer);
 
-          const closeButton = detailsContainer.querySelector(".close-btn");
-          closeButton.addEventListener("click", () => {
+          const closeButton = detailsContainer.querySelector('.close-btn');
+          closeButton.addEventListener('click', () => {
             toggleDetailsVisibility(detailsContainer);
           });
         });
